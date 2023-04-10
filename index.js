689bf60a64ec8daa967731b9591ad8569c08a722
james <astonjames@iahni.com>
2023-04-09 19:28:43 -0600

    complete

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Write your code here!
 main.remove();
-const newHeader = document.createElement('h1');
-newHeader.setAttribute("id", 'victory');
-document.body.append(newHeader);
-const h1 =document.querySelector('h1');
-h1.textContent = 'james is the champion';
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = " james is the champion";
