bf5ab490b5f6f6cfe3ee02aa345df9b773406119
james <astonjames@iahni.com>
2023-04-09 19:22:10 -0600

    complete?

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", 'victory');
+document.body.append(newHeader);
+const h1 =document.querySelector('h1');
+h1.textContent = 'james is the champion';
